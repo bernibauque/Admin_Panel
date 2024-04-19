e865181cc2c9132fa2f99923336303cffb91b785
Bauque Bernardita <bernibauque@gmail.com>
2024-04-19 13:58:56 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           <Route path='list-color' element={<Colorlist />} />
           <Route path='list-category' element={<Categorylist />} />
           <Route path='list-brand' element={<Brandlist />} />
-          <Route path='list-product' element={<Productlist />} />
+          <Route path='product-list' element={<Productlist />} />
         </Route>
       </Routes>
     </Router>
