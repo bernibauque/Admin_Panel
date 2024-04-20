341bd8091a99280617e9a10e10476cac67996249
Bauque Bernardita <bernibauque@gmail.com>
2024-04-20 16:38:01 -0300

    Agregando Addbrand

---
+++ src/pages/Addbrand.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CustomInput from "../components/CustomInput";
+
+const Addbrand = () => {
+    return (
+        <div>
+            <h3 className='mb-4'>Agregar Marca</h3>
+            <div>
+                <form action=''>
+                    <CustomInput type='text' label='Ingrese Marca' />
+                    <button
+                        className='btn btn-success border-0 rounded-3 my-5'
+                        type='submit'
+                    >
+                        Agregar Marca
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Addbrand;
