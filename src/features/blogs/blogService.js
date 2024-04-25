c2d0a004df66547e1b59a4653bfba5eb448881dc
Bauque Bernardita <bernibauque@gmail.com>
2024-04-25 02:01:36 -0300

    Cambios BlogService

--- src/features/blogs/blogService.js
+++ src/features/blogs/blogService.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { base_url } from "../../utils/base_url";
 
-const getBrands = async () => {
-    const response = await axios.get(`${base_url}brand/`);
+const getBlogs = async () => {
+    const response = await axios.get(`${base_url}blog/`);
     return response.data;
 };
 
-const brandService = {
-    getBrands,
+const blogService = {
+    getBlogs,
 };
-export default brandService;
+export default blogService;
