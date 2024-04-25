9fe857aece6a6eca29a4174ee93f864ab42039c2
Bauque Bernardita <bernibauque@gmail.com>
2024-04-25 00:38:36 -0300

    Agregando data real en Productlist

--- src/pages/Productlist.js
+++ src/pages/Productlist.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from "antd";
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
+import { getProducts } from '../features/product/productSlice';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import Link from 'antd/es/typography/Link';
 
 const columns = [
     {
@@ -9,29 +13,52 @@ const columns = [
         dataIndex: 'key',
     },
     {
-        title: 'Name',
-        dataIndex: 'name',
+        title: 'Titulo',
+        dataIndex: 'title',
     },
     {
-        title: 'Product',
-        dataIndex: 'product',
+        title: 'Marca',
+        dataIndex: 'brand',
     },
     {
-        title: 'Status',
-        dataIndex: 'status',
+        title: 'Categoria',
+        dataIndex: 'category',
+    },
+    {
+        title: 'Precio',
+        dataIndex: 'price',
+    },
+    {
+        title: 'Accion',
+        dataIndex: 'action',
     },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-    data1.push({
-        key: i,
-        name: `Edward King ${i}`,
-        product: 32,
-        status: `London, Park Lane no. ${i}`,
-    });
-}
 
 const Productlist = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getProducts());
+    }, []);
+    const productState = useSelector((state) => state.product.products);
+    const data1 = [];
+    for (let i = 0; i < productState.length; i++) {
+        data1.push({
+            key: i + 1,
+            title: productState[i].title,
+            brand: productState[i].brand,
+            category: productState[i].category,
+            price: `$ ${productState[i].price}`,
+            action:
+                <>
+                    <Link className='fs-3 text-danger' to='/'>
+                        <BiEdit />
+                    </Link>
+                    <Link className='ms-3 fs-3 text-danger' to='/'>
+                        <AiFillDelete />
+                    </Link>
+                </>
+        });
+    }
     return (
         <div>
             <h3 className='mb-4 title'>Listado de Productos</h3>
