e0e044c9de1ad0f226d731e32ecb3ae0da56df70
Bauque Bernardita <bernibauque@gmail.com>
2024-04-25 16:46:54 -0300

    Finalizacion enquiries

--- src/pages/Enquiries.js
+++ src/pages/Enquiries.js
@@ -1,35 +1,68 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from "antd";
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { getEnquiries } from '../features/enquiry/enquirySlice';
 
 const columns = [
     {
-        title: 'SNo',
+        title: 'Numero',
         dataIndex: 'key',
     },
     {
-        title: 'Name',
+        title: 'Nombre',
         dataIndex: 'name',
     },
     {
-        title: 'Product',
-        dataIndex: 'product',
+        title: 'Email',
+        dataIndex: 'email',
     },
     {
-        title: 'Status',
+        title: 'Telefono',
+        dataIndex: 'mobile',
+    },
+    {
+        title: 'Estado',
         dataIndex: 'status',
     },
+    {
+        title: 'Accion',
+        dataIndex: 'action',
+    },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-    data1.push({
-        key: i,
-        name: `Edward King ${i}`,
-        product: 32,
-        status: `London, Park Lane no. ${i}`,
-    });
-}
 
 const Enquiries = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getEnquiries());
+    }, []);
+    const enqState = useSelector((state) => state.enquiry.enquiries);
+    const data1 = [];
+    for (let i = 0; i < enqState.length; i++) {
+        data1.push({
+            key: i + 1,
+            name: enqState[i].name,
+            email: enqState[i].email,
+            mobile: enqState[i].mobile,
+            status: (
+                <>
+                    <select name='' className='form-control form-select' id=''>
+                        <option value=''>Establecer Estado</option>
+                    </select>
+                </>
+            ),
+            action: (
+                <>
+                    <Link className='ms-3 fs-3 text-danger' to='/'>
+                        <AiFillDelete />
+                    </Link>
+                </>
+            ),
+        });
+    }
     return (
         <div>
             <h3 className='mb-4 title'>Consultas</h3>
