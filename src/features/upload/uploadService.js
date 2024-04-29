52c72a0c8ae4fd7e665b6938329482031e976b5d
Bauque Bernardita <bernibauque@gmail.com>
2024-04-28 21:10:54 -0300

    Cambios uploadService

---
+++ src/features/upload/uploadService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { config } from "../../utils/axiosconfig";
+import { base_url } from "../../utils/base_url";
+
+const uploadImg = async (data) => {
+    const response = await axios.post(`${base_url}`, data, config);
+    return response.data;
+};
+
+const uploadService = {
+    uploadImg,
+};
+
+export default uploadService;
