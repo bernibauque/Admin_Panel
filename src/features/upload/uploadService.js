cfeef2773805478ff0020c17ab44796b83c0e7de
Bauque Bernardita <bernibauque@gmail.com>
2024-04-29 21:42:54 -0300

    Agregando deleteImg

--- src/features/upload/uploadService.js
+++ src/features/upload/uploadService.js
@@ -6,9 +6,14 @@ const uploadImg = async (data) => {
     const response = await axios.post(`${base_url}upload/`, data, config);
     return response.data;
 };
+const deleteImg = async (id) => {
+    const response = await axios.delete(`${base_url}upload/`, id, config);
+    return response.data;
+};
 
 const uploadService = {
     uploadImg,
+    deleteImg
 };
 
 export default uploadService;
