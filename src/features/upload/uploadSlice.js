100b419de3c39c0a9554afef2314d43a38760c0e
Bauque Bernardita <bernibauque@gmail.com>
2024-04-29 21:46:40 -0300

    Agregando delImg

--- src/features/upload/uploadSlice.js
+++ src/features/upload/uploadSlice.js
@@ -13,7 +13,18 @@ export const uploadImg = createAsyncThunk(
         } catch (error) {
             return thunkAPI.rejectWithValue(error);
         }
-    });
+    }
+);
+export const delImg = createAsyncThunk(
+    "delete/images",
+    async (id, thunkAPI) => {
+        try {
+            return await uploadService.deleteImg(id);
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error);
+        }
+    }
+);
 const initialState = {
     images: [],
     isError: false,
@@ -41,7 +52,22 @@ export const uploadSlice = createSlice({
                 state.isError = true;
                 state.isSuccess = false;
                 state.message = action.error;
-            });
+            })
+            .addCase(delImg.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(uploadImg.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isError = false;
+                state.isSuccess = true;
+                state.images = action.payload;
+            })
+            .addCase(uploadImg.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.isSuccess = false;
+                state.message = action.payload;
+            })
     },
 });
 
