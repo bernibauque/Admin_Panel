82d53c6bbee7129e56f3b3187ec2de0eee6ff559
Bauque Bernardita <bernibauque@gmail.com>
2024-05-05 21:41:41 -0300

    Agregando createBloCat

--- src/features/bcategory/bcategoryService.js
+++ src/features/bcategory/bcategoryService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { config } from "../../utils/axiosconfig";
 import { base_url } from "../../utils/base_url";
 
 const getBlogCategories = async () => {
@@ -6,7 +7,13 @@ const getBlogCategories = async () => {
     return response.data;
 };
 
+const createBlogCategory = async (bcat) => {
+    const response = await axios.post(`${base_url}blogcategory/`, bcat, config);
+    return response.data;
+};
+
 const bcategoryService = {
     getBlogCategories,
+    createBlogCategory
 };
 export default bcategoryService;
