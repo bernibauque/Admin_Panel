44cc365464f82c5c2a2cd58fb888cd64cda3dc23
Bauque Bernardita <bernibauque@gmail.com>
2024-05-12 16:54:51 -0300

    Agregar Coupon listo

--- src/app/store.js
+++ src/app/store.js
@@ -1,38 +1,3 @@
-/*import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import authReducer from "../features/auth/authSlice";
-import customerReducer from "../features/customers/customerSlice";
-import productReducer from "../features/product/productSlice";
-import brandReducer from "../features/brand/brandSlice";
-import pcategoryReducer from "../features/pcategory/pcategorySlice";
-import bcategoryReducer from "../features/bcategory/bcategorySlice";
-import blogReducer from "../features/blogs/blogSlice";
-import enquiryReducer from "../features/enquiry/enquirySlice";
-import uploadReducer from "../features/upload/uploadSlice";
-
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        customer: customerReducer,
-        product: productReducer,
-        brand: brandReducer,
-        pCategory: pcategoryReducer,
-        bCategory: bcategoryReducer,
-        blogs: blogReducer,
-        enquiry: enquiryReducer,
-        upload: uploadReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: ['createNewblogcat/fulfilled', 'auth/login/rejected'],
-                ignoredPaths: ['payload'],
-            },
-        }),
-});
-
-export default store;*/
-
-/* ORIGINAL*/
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/customers/customerSlice";
@@ -43,6 +8,7 @@ import bcategoryReducer from "../features/bcategory/bcategorySlice";
 import blogReducer from "../features/blogs/blogSlice";
 import enquiryReducer from "../features/enquiry/enquirySlice";
 import uploadReducer from "../features/upload/uploadSlice";
+import couponReducer from "../features/coupon/couponSlice"
 
 export const store = configureStore({
     reducer: {
@@ -55,5 +21,6 @@ export const store = configureStore({
         blogs: blogReducer,
         enquiry: enquiryReducer,
         upload: uploadReducer,
+        coupon: couponReducer,
     },
 });
--- src/pages/AddCoupon.js
+++ src/pages/AddCoupon.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import { React, useEffect } from 'react';
+import CustomInput from "../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { createCoupon, resetState } from '../features/coupon/couponSlice';
+
+let schema = yup.object().shape({
+    name: yup.string().required("Es necesario colocar el nombre del cupon."),
+    expiry: yup.date().required("Es necesario colocar los datos de caducidad."),
+    discount: yup.number().required("Es necesario colocar el porcentaje de descuento."),
+});
 
 const AddCoupon = () => {
+    const dispatch = useDispatch();
+    const newCoupon = useSelector((state) => state.coupon);
+    const { isSuccess, isError, isLoading, createdCoupon } = newCoupon;
+
+    useEffect(() => {
+        if (isSuccess && createdCoupon) {
+            toast.success("Cupon agregado con Exito!");
+        }
+        if (isError) {
+            toast.error("Algo salio mal!");
+        }
+    }, [isSuccess, isError, isLoading]);
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            expiry: "",
+            discount: "",
+        },
+        validationSchema: schema,
+        onSubmit: (values) => {
+            dispatch(createCoupon(values));
+            formik.resetForm();
+            setTimeout(() => {
+                dispatch(resetState);
+            }, 3000);
+        },
+    });
+
     return (
-        <div>AddCoupon</div>
+        <div>
+            <h3 className='mb-4 title'>Agregar Cupon</h3>
+            <div>
+                <form action='' onSubmit={formik.handleSubmit}>
+                    <CustomInput
+                        type='text'
+                        name='name'
+                        onCh={formik.handleChange("name")}
+                        onBlr={formik.handleBlur("name")}
+                        val={formik.values.name}
+                        label='Ingrese nombre del Cupon'
+                        id="name"
+                    />
+                    <div className='error'>
+                        {formik.touched.name && formik.errors.name}
+                    </div>
+                    <CustomInput
+                        type='date'
+                        name='expiry'
+                        onCh={formik.handleChange("expiry")}
+                        onBlr={formik.handleBlur("expiry")}
+                        val={formik.values.name}
+                        label='Ingrese datos de caducidad'
+                        id="date"
+                    />
+                    <div className='error'>
+                        {formik.touched.expiry && formik.errors.expiry}
+                    </div>
+                    <CustomInput
+                        type='number'
+                        name='discount'
+                        onCh={formik.handleChange("discount")}
+                        onBlr={formik.handleBlur("discount")}
+                        val={formik.values.discount}
+                        label='Ingrese descuento'
+                        id="discount"
+                    />
+                    <div className='error'>
+                        {formik.touched.discount && formik.errors.discount}
+                    </div>
+                    <button
+                        className='btn btn-success border-0 rounded-3 my-5'
+                        type='submit'
+                    >
+                        Agregar Cupon
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
 
-export default AddCoupon
+export default AddCoupon;
