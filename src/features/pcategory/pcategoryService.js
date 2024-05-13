6fe7be8e6a1ef28803b26ecd485b52e018a87096
Bauque Bernardita <bernibauque@gmail.com>
2024-05-13 19:26:37 -0300

    Agregando deleteProductCategory al service

--- src/features/pcategory/pcategoryService.js
+++ src/features/pcategory/pcategoryService.js
@@ -6,13 +6,27 @@ const getProductCategories = async () => {
     const response = await axios.get(`${base_url}category/`);
     return response.data;
 };
+
 const createCategory = async (category) => {
     const response = await axios.post(`${base_url}category/`, category, config);
+    return response.data;
+};
+
+const getProductCategory = async (id) => {
+    const response = await axios.get(`${base_url}category/${id}`, config);
+    return response.data;
+};
+
+const deleteProductCategory = async (id) => {
+    const response = await axios.delete(`${base_url}category/${id}`, config);
     return response.data;
 };
 
 const pCategoryService = {
     getProductCategories,
     createCategory,
+    getProductCategory,
+    deleteProductCategory
 };
+
 export default pCategoryService;
