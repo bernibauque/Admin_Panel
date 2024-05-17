90084285908d14efbb9a001179e6f9051e62e9b9
Bauque Bernardita <bernibauque@gmail.com>
2024-05-17 12:30:34 -0300

    Cambios en el service para delete update, get (no definitivos)

--- src/features/blogs/blogService.js
+++ src/features/blogs/blogService.js
@@ -12,8 +12,30 @@ const createBlog = async (blog) => {
     return response.data;
 };
 
+const updateBlog = async (brand) => {
+    const response = await axios.put(
+        `${base_url}blog/${blog.id}`,
+        { title: brand.brandData.title },
+        config
+    );
+    return response.data;
+};
+
+const getBlog = async (id) => {
+    const response = await axios.get(`${base_url}blog/${id}`, config);
+    return response.data;
+};
+
+const deleteBlog = async (id) => {
+    const response = await axios.delete(`${base_url}blog/${id}`, config);
+    return response.data;
+};
+
 const blogService = {
     getBlogs,
-    createBlog
+    createBlog,
+    getBlog,
+    updateBlog,
+    deleteBlog,
 };
 export default blogService;
