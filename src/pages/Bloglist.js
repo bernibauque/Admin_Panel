f4ae3a79f521c6c9bac122f6e4b2fec48f7dccef
Bauque Bernardita <bernibauque@gmail.com>
2024-05-17 12:26:59 -0300

    modificaciones para delete

--- src/pages/Bloglist.js
+++ src/pages/Bloglist.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table } from "antd";
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
-import { getBlogs } from '../features/blogs/blogSlice';
+import { getBlogs, resetState } from '../features/blogs/blogSlice';
+import CustomModal from '../components/CustomModal';
 
 const columns = [
     {
@@ -27,29 +28,49 @@ const columns = [
 ];
 
 const Bloglist = () => {
+    const [open, setOpen] = useState(false);
+    const [blogId, setblogId] = useState("");
+    const showModal = (e) => {
+        setOpen(true);
+        setblogId(e);
+    };
+    const hideModal = () => {
+        setOpen(false);
+    };
     const dispatch = useDispatch();
     useEffect(() => {
+        dispatch(resetState());
         dispatch(getBlogs());
     }, []);
-    const getBlogsState = useSelector((state) => state.blogs.blogs);
+    const getBlogState = useSelector((state) => state.blogs.blogs);
     const data1 = [];
-    for (let i = 0; i < getBlogsState.length; i++) {
+    for (let i = 0; i < getBlogState.length; i++) {
         data1.push({
             key: i + 1,
-            name: getBlogsState[i].title,
-            category: getBlogsState[i].category,
-            status: `London, Park Lane no. ${i}`,
+            name: getBlogState[i].title,
+            category: getBlogState[i].category,
+
             action: (
                 <>
-                    <Link to='/' className='fs-3 text-danger'>
+                    <Link to={`/admin/blog/${getBlogState[i].id}`} className='fs-3 text-danger'>
                         <BiEdit />
                     </Link>
-                    <Link className='ms-3 fs-3 text-danger' to='/'>
+                    <button
+                        className='ms-3 fs-3 text-danger bg-transparent border-0'
+                        onClick={() => showModal(getBlogState[i]._id)}
+                    >
                         <AiFillDelete />
-                    </Link>
+                    </button>
                 </>
             ),
         });
+    }
+    const deleteBlog = (e) => {
+        //dispatch(deleteABrand(e))
+        setOpen(false);
+        setTimeout(() => {
+            dispatch(getBlogs());
+        }, 100);
     }
     return (
         <div>
@@ -57,6 +78,14 @@ const Bloglist = () => {
             <div>
                 <Table columns={columns} dataSource={data1} />
             </div>
+            <CustomModal
+                hideModal={hideModal}
+                open={open}
+                performAction={() => {
+                    deleteBlog(blogId);
+                }}
+                title='Estas seguro de que deseas eliminar este Blog?'
+            />
         </div>
     )
 }
